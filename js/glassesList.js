b23212d0e04a496eb4641846647aca06e77dec34
phucnx-IT <phucnx.phar@gmail.com>
2024-12-25 17:08:34 +0700

    first_commit

---
+++ js/glassesList.js
@@ -0,0 +1,20 @@
+export class GlassesList {
+        constructor() {
+                this.glasses = [];
+        }
+
+        addGlasses(glasses) {
+                this.glasses.push(glasses);
+        }
+
+        renderGlasses() {
+                let content = '';
+                content = this.glasses.reduce((glassContent, item, index) => {
+                        glassContent += `<div class="col-4">
+                                <img class="img-fluid" src="${item.src}" alt="${item.name}">
+                        </div>`
+                        return glassContent;
+                }, '');
+                return content;
+        }
+}
